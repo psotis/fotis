b9b7b9d91caa5104ee436ced093f52e9797bff77
Fotis Psarris <77319413+psotis@users.noreply.github.com>
2023-11-28 01:37:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
+    const pdfPath = '/path/to/yourfile.pdf';
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
@@ -15,6 +16,12 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+      <iframe
+          src={pdfPath}
+          width="100%"
+          height="600px"
+          style={{ border: 'none' }}
+        />
       </main>
 
       <Footer />
