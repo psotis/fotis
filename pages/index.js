bda7770e21e183d070d5812f8bf60db395b84072
Fotis Psarris <77319413+psotis@users.noreply.github.com>
2023-11-28 01:32:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,23 @@
-// index.js
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-document.addEventListener('DOMContentLoaded', function () {
-  // This ensures the HTML is injected after the document has fully loaded.
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-  // Replace 'yourfile.pdf' with the actual path to your PDF file
-  const pdfPath = 'path/to/yourfile.pdf';
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
 
-  // PDF.js setup
-  const pdfjsLib = window['pdfjs-dist/build/pdf'];
-
-  // The workerSrc property should be set to the location of the PDF.js worker file
-  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://unpkg.com/pdfjs-dist@2.7.570/build/pdf.worker.min.js';
-
-  // Fetch the PDF document
-  pdfjsLib.getDocument(pdfPath).then(function (pdfDocument) {
-    // Get the first page of the PDF
-    pdfDocument.getPage(1).then(function (pdfPage) {
-      // Set up the canvas element
-      const canvas = document.createElement('canvas');
-      const context = canvas.getContext('2d');
-      const viewport = pdfPage.getViewport({ scale: 1 });
-
-      // Set the canvas size to match the PDF page
-      canvas.width = viewport.width;
-      canvas.height = viewport.height;
-
-      // Append the canvas to the body
-      document.body.appendChild(canvas);
-
-      // Render the PDF page to the canvas
-      pdfPage.render({ canvasContext: context, viewport: viewport });
-    });
-  });
-});
+      <Footer />
+    </div>
+  )
+}
