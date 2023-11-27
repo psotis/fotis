bac7388741c4b62ecaba7e1d96a6ee696ce49883
Fotis Psarris <77319413+psotis@users.noreply.github.com>
2023-11-28 01:30:05 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-<!-- index.html -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Netlify PDF Example</title>
-</head>
-<body>
+// index.js
 
-<iframe src="cv.pdf" width="100%" height="600px"></iframe>
+document.addEventListener('DOMContentLoaded', function () {
+  // This ensures the HTML is injected after the document has fully loaded.
 
-</body>
-</html>
+  // Replace 'yourfile.pdf' with the actual path to your PDF file
+  const pdfPath = 'path/to/yourfile.pdf';
+
+  // PDF.js setup
+  const pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+  // The workerSrc property should be set to the location of the PDF.js worker file
+  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://unpkg.com/pdfjs-dist@2.7.570/build/pdf.worker.min.js';
+
+  // Fetch the PDF document
+  pdfjsLib.getDocument(pdfPath).then(function (pdfDocument) {
+    // Get the first page of the PDF
+    pdfDocument.getPage(1).then(function (pdfPage) {
+      // Set up the canvas element
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+      const viewport = pdfPage.getViewport({ scale: 1 });
+
+      // Set the canvas size to match the PDF page
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+
+      // Append the canvas to the body
+      document.body.appendChild(canvas);
+
+      // Render the PDF page to the canvas
+      pdfPage.render({ canvasContext: context, viewport: viewport });
+    });
+  });
+});
