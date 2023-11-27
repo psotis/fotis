7e60e18a7a08c3d651ad1d4a6af9946a4cbea81f
Fotis Psarris <77319413+psotis@users.noreply.github.com>
2023-11-28 01:48:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,6 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       <iframe
           src={'/cv.pdf'}
           width="100%"
