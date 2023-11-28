ef2367dfe70b7f1eb5b7ebfa77911f9901e16490
Fotis Psarris <77319413+psotis@users.noreply.github.com>
2023-11-28 02:15:39 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,23 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Fotis CV</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
       <iframe
-  
           src={"https://rawcdn.githack.com/psotis/fotis/main/cv.pdf"}
           width="900px"
           height="600px"
           style={{ border: 'none' }}
         />
       </main>
-
-      <Footer />
+          
     </div>
   )
 }
